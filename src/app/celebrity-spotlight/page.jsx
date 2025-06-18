d7d896b45475b64c30b865adfaeb9ee1a0b5eac1
muskan manjhi <muskanmanjhi15@gmail.com>
2025-06-18 21:57:02 +0530

    Refactor import paths for ProductGrid and ProductFilters to lowercase

--- src/app/celebrity-spotlight/page.jsx
+++ src/app/celebrity-spotlight/page.jsx
@@ -1,7 +1,7 @@
 
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import ProductGrid from "@/components/ProductGrid";
+import ProductGrid from "@/components/productgrid";
 
 const celebrities = [
   {
